efa25918e0e1714c02d267797a26afe6601b7719
Pakker7 <43773175+Pakker7@users.noreply.github.com>
2020-07-30 22:43:19 +0900

    전역 변수 선언 까지 해봄

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import StatusComponent from './status'
+
+Vue.component('AppStatus',StatusComponent)/* 전역변수로 선언 */
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
+new Vue({ /*vue를 선언, 이 선언된 곳을 통해서 전체 애플리케이션이 실행됨*/
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
